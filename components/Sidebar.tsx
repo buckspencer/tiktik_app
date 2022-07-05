4a423f74a6a5fa5e009fbe361923809d260dfb56
Buck Spencer <buck.spencer@mutations.ltd>
2022-07-05 10:13:30 -0700

    Add video card

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,9 @@ import Link from "next/link";
 import GoogleLogin from "react-google-login";
 import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
 import { ImCancelCircle } from "react-icons/im";
+import Discover from "./Discover";
+import SuggestedAccounts from "./SuggestedAccounts";
+import Footer from "./Footer";
 
 const Sidebar = () => {
   const [showSidebar, setShowSidebar] = useState(true);
@@ -22,42 +25,23 @@ const Sidebar = () => {
       >
         {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
       </div>
-      <div className="xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
-        <div className="xl:border-b-2 border-gray-200 xl:pb-4">
-          <Link href="/">
-            <div className={normalLink}>
-              <p className="text-2xl">
-                <AiFillHome />
-              </p>
-              <span className="text-xl hidden xl:block">For you</span>
-            </div>
-          </Link>
-        </div>
-        {!userProfile && (
-          <div className="px-2 py-4 hidden xl:block">
-            <p className="text-gray-400">
-              Log in to like and comment on videos.
-            </p>
-            <div className="pr-4">
-              <GoogleLogin
-                clientId=""
-                render={(renderProps) => (
-                  <button
-                    className="bg-white text-lg text-[#F51997]"
-                    onClick={renderProps.onClick}
-                    disabled={renderProps.disabled}
-                  >
-                    Log in
-                  </button>
-                )}
-                onSuccess={() => {}}
-                onFailure={() => {}}
-                cookiePolicy="single_host_origin"
-              />
-            </div>
+      {showSidebar && (
+        <div className="xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
+          <div className="xl:border-b-2 border-gray-200 xl:pb-4">
+            <Link href="/">
+              <div className={normalLink}>
+                <p className="text-2xl">
+                  <AiFillHome />
+                </p>
+                <span className="text-xl hidden xl:block">For you</span>
+              </div>
+            </Link>
           </div>
-        )}
-      </div>
+          <Discover />
+          <SuggestedAccounts />
+          <Footer />
+        </div>
+      )}
     </div>
   );
 };
