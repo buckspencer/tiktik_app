e6e264bfa200bdabf24a824bcdbb6e98f1a4318c
Buck Spencer <buck.spencer@mutations.ltd>
2022-07-12 15:31:14 -0700

    Add SuggestedAccounts

--- store/authStore.ts
+++ store/authStore.ts
@@ -1,12 +1,19 @@
+import { BASE_URL } from "../utils";
+import axios from "axios";
 import create from "zustand"
 import {persist} from "zustand/middleware"
-import axios from "axios";
 
 const authStore = (set: any, addUser: any) => ({
   userProfile: null,
+  allUsers: [],
 
   addUser: (user: any) => set({userProfile: user}),
-  removeUser: () => set({userProfile : null})
+  removeUser: () => set({userProfile : null}),
+  fetchAllUsers: async () => {
+    const response = await axios.get(`${BASE_URL}/api/users`);
+
+    set({allUsers: response.data});
+  }
 });
 
 
