2d614aafff4b91e39b497db86ee540b9d0f94228
Buck Spencer <buck.spencer@mutations.ltd>
2022-07-13 11:42:29 -0700

    Clean up comments section

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -32,7 +32,7 @@ const Comments = ({
   const { userProfile, allUsers }: any = useAuthStore();
 
   return (
-    <div className="border-t-2 border-gray-200 pt-4 px-10 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]">
+    <div className="border-t-2 border-gray-200 px-10 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]">
       <div className="overflow-scroll lg:h-[475px]">
         {comments?.length > 0 ? (
           comments?.map((item: IComment, idx: number) => (
@@ -73,7 +73,7 @@ const Comments = ({
             </>
           ))
         ) : (
-          <NoResults text="No comments yet! Be the first one to add a comment." />
+          <NoResults text="No comments yet!" />
         )}
       </div>
 
